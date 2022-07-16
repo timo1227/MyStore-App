fd4013584b0eee695bf6df12ddd21247016b5351
Timothy Estrada <43392928+timo1227@users.noreply.github.com>
2022-07-16 16:15:44 +0000

    session settings for memcached

--- partials/nav.php
+++ partials/nav.php
@@ -1,5 +1,16 @@
 <?php
 require_once(__DIR__ . "/../lib/functions.php");
+ini_set('session.save_handler', 'memcached');
+ini_set('session.save_path', getenv('MEMCACHIER_SERVERS'));
+if (version_compare(phpversion('memcached'), '3', '>=')) {
+    ini_set('memcached.sess_persistent', 1);
+    ini_set('memcached.sess_binary_protocol', 1);
+} else {
+    ini_set('session.save_path', 'PERSISTENT=myapp_session ' . ini_get('session.save_path'));
+    ini_set('memcached.sess_binary', 1);
+}
+ini_set('memcached.sess_sasl_username', getenv('MEMCACHIER_USERNAME'));
+ini_set('memcached.sess_sasl_password', getenv('MEMCACHIER_PASSWORD'));
 //Note: this is to resolve cookie issues with port numbers
 $domain = $_SERVER["HTTP_HOST"];
 if (strpos($domain, ":")) {
