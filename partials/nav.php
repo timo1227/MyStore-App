caadb2d2bc35a09ed635467187cc04a4afcf8029
Timothy Estrada <43392928+timo1227@users.noreply.github.com>
2022-07-16 17:15:29 +0000

    session settings for memcached

--- partials/nav.php
+++ partials/nav.php
@@ -1,5 +1,44 @@
 <?php
 require_once(__DIR__ . "/../lib/functions.php");
+require 'vendor/autoload.php';
+
+// create a new persistent client
+$m = new Memcached("memcached_pool");
+$m->setOption(Memcached::OPT_BINARY_PROTOCOL, TRUE);
+
+// some nicer default options
+// - nicer TCP options
+$m->setOption(Memcached::OPT_TCP_NODELAY, TRUE);
+$m->setOption(Memcached::OPT_NO_BLOCK, FALSE);
+// - timeouts
+$m->setOption(Memcached::OPT_CONNECT_TIMEOUT, 2000);    // ms
+$m->setOption(Memcached::OPT_POLL_TIMEOUT, 2000);       // ms
+$m->setOption(Memcached::OPT_RECV_TIMEOUT, 750 * 1000); // us
+$m->setOption(Memcached::OPT_SEND_TIMEOUT, 750 * 1000); // us
+// - better failover
+$m->setOption(Memcached::OPT_DISTRIBUTION, Memcached::DISTRIBUTION_CONSISTENT);
+$m->setOption(Memcached::OPT_LIBKETAMA_COMPATIBLE, TRUE);
+$m->setOption(Memcached::OPT_RETRY_TIMEOUT, 2);
+$m->setOption(Memcached::OPT_SERVER_FAILURE_LIMIT, 1);
+$m->setOption(Memcached::OPT_AUTO_EJECT_HOSTS, TRUE);
+
+// setup authentication
+$m->setSaslAuthData(
+    getenv("MEMCACHIER_USERNAME"),
+    getenv("MEMCACHIER_PASSWORD")
+);
+
+// We use a consistent connection to memcached, so only add in the
+// servers first time through otherwise we end up duplicating our
+// connections to the server.
+if (!$m->getServerList()) {
+    // parse server config
+    $servers = explode(",", getenv("MEMCACHIER_SERVERS"));
+    foreach ($servers as $s) {
+        $parts = explode(":", $s);
+        $m->addServer($parts[0], $parts[1]);
+    }
+}
 //Note: this is to resolve cookie issues with port numbers
 $domain = $_SERVER["HTTP_HOST"];
 if (strpos($domain, ":")) {
@@ -77,12 +116,3 @@ session_start();
         </div>
     </div>
 </nav>
-
-<?php
-
-if (!isset($_SESSION['count'])) {
-    $_SESSION['count'] = 0;
-}
-$_SESSION['count']++;
-
-echo "Hello #" . $_SESSION['count'];
