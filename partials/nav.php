4df6326814ebf8e1eb48118698a8d626eeb41d42
Tim <tim_1227@aol.com>
2022-07-15 20:01:22 -0400

    Milestone2

--- partials/nav.php
+++ partials/nav.php
@@ -24,8 +24,10 @@ session_start();
 
 ?>
 <!-- include css and js files -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <link rel="stylesheet" href="<?php echo get_url('Styles/nav.css'); ?>">
 <link rel="stylesheet" href="<?php echo get_url('Styles/body.css'); ?>">
+<script src="<?php echo get_url('Scripts/helpers.js'); ?>"></script>
 <nav>
     <ul>
         <?php if (is_logged_in()) : ?>
