832392eaaf045587476dc5c36fe9a2f759311000
Timothy Estrada <43392928+timo1227@users.noreply.github.com>
2022-07-16 19:47:49 +0000

    Memcached Settings

--- partials/nav.php
+++ partials/nav.php
@@ -37,25 +37,25 @@ if (!$m->getServerList()) {
 
 
 //Note: this is to resolve cookie issues with port numbers
-$domain = $_SERVER["HTTP_HOST"];
-if (strpos($domain, ":")) {
-    $domain = explode(":", $domain)[0];
-}
-$localWorks = true; //some people have issues with localhost for the cookie params
-//if you're one of those people make this false
+// $domain = $_SERVER["HTTP_HOST"];
+// if (strpos($domain, ":")) {
+//     $domain = explode(":", $domain)[0];
+// }
+// $localWorks = true; //some people have issues with localhost for the cookie params
+// //if you're one of those people make this false
 
-//this is an extra condition added to "resolve" the localhost issue for the session cookie
-if (($localWorks && $domain == "localhost") || $domain != "localhost") {
-    session_set_cookie_params([
-        "lifetime" => 60 * 60,
-        "path" => "$BASE_PATH",
-        //"domain" => $_SERVER["HTTP_HOST"] || "localhost",
-        "domain" => $domain,
-        "secure" => true,
-        "httponly" => true,
-        "samesite" => "lax"
-    ]);
-}
+// //this is an extra condition added to "resolve" the localhost issue for the session cookie
+// if (($localWorks && $domain == "localhost") || $domain != "localhost") {
+//     session_set_cookie_params([
+//         "lifetime" => 60 * 60,
+//         "path" => "$BASE_PATH",
+//         //"domain" => $_SERVER["HTTP_HOST"] || "localhost",
+//         "domain" => $domain,
+//         "secure" => true,
+//         "httponly" => true,
+//         "samesite" => "lax"
+//     ]);
+// }
 session_start();
 $_SESSION['test'] = 42;
 // check if session info saved in memcached
