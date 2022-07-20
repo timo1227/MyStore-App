075b95e694c7f877e4a99a4eb7b28c9ed2ba7417
Tim <tim_1227@aol.com>
2022-07-20 16:34:40 -0400

    HTACCESS TEST

--- partials/nav.php
+++ partials/nav.php
@@ -61,6 +61,7 @@ if (file_exists('/app/vendor/autoload.php')) {
 ?>
 <!-- include css and js files -->
 <title>My Shop</title>
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
